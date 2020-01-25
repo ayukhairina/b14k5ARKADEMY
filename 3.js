169c243574b1aa860ba0390b2b806935e08fd9f0
ayukhairina <58674812+ayukhairina@users.noreply.github.com>
2020-01-25 21:44:24 +0800

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,13 @@
+function jabat(x){
+    let nilai = 0;
+    for(let i =  x-1 ; i>=1; i --){
+        nilai += i;
+        }
+      
+        return nilai;
+    }
+    
+
+
+console.log(jabat(3));
+console.log(jabat(6));
